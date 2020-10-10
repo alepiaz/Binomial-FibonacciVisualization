aa3b32cc4e63e51c4285ab27e47a58846d14170a
Alessio <alexsquare9511@gmail.com>
2020-10-10 18:47:22 +0200

    fixed extract min

--- newBH.py
+++ newBH.py
@@ -8,68 +8,85 @@ DOT = """digraph G {
 
 class Node:
 
-    def __init__(self):
-        self.n = None
+    def __init__(self, k = None):
+        self.n = k
         self.degree = 0
         self.parent = None
         self.child = None
-        self.sibiling = None
+        self.sibling = None
+
+    def findMinNode(self):
+        x = y = self
+
+        min = x.n
+        while (x != None):
+            if (x.n < min):
+                y = x
+                min = x.n
+            x = x.sibling
+        return y
+
+    def reverse(self,sib):
+        ret = Node()
+        if (self.sibling != None):
+            ret = self.sibling.reverse(self)
+        else:
+            ret = self
+        sibling = sib
+        return ret
 
 class BinomialHeap:
 
-    # def __init__(self):
-    #     self.H = self.initializeHeap()
-    #     self.Hr = self.initializeHeap()
-    #     self.count = 1
+    def __init__(self):
+        self.H = None
+        self.Hr = None
 
 
-    def createNode(self,k):
-        p = Node()
-        p.n = k
-        return p
 
-    def initializeHeap(self):
-        return None
+
+    # def initializeHeap(self):
+    #     return None
 
     def binomialLink(self,y,z):
         y.parent = z
-        y.sibiling = z.child
+        y.sibling = z.child
         z.child = y
         z.degree +=1
 
-    def insert(self,H,x):
-        n = self.createNode(x)
-        H = self.union(H ,n)
-        return H
+    def insert(self,x):
+        n = Node(x)
+        self.union(self.H,n)
+        return self
 
     def union(self,H1,H2):
         H = self.merge(H1,H2)
         if H == None:
-            return H
+            self.H = H
+            return
         prev = None
         x = H
-        next = x.sibiling
+        next = x.sibling
         while(next != None):
-            if ((x.degree != next.degree) or ((next.sibiling != None) and next.sibiling.degree == x.degree)):
+            if ((x.degree != next.degree) or ((next.sibling != None) and next.sibling.degree == x.degree)):
                 prev = x
                 x = next
             else:
                 if (x.n <= next.n):
-                    x.sibiling = next.sibiling
+                    x.sibling = next.sibling
                     self.binomialLink(next,x)
                 else:
                     if (prev == None):
                         H = next
                     else:
-                        prev.sibiling = next
+                        prev.sibling = next
                     self.binomialLink(x,next)
                     x = next
-            next = x.sibiling
+            next = x.sibling
 
-        return H
+        self.H = H
 
     def merge(self,H1,H2):
-        H = self.initializeHeap()
+        H = None
         y = H1
         z = H2
         if (y != None):
@@ -85,58 +102,100 @@ class BinomialHeap:
 
         while (y != None and z != None):
             if (y.degree < z.degree):
-                y = y.sibiling
+                y = y.sibling
             elif (y.degree == z.degree):
-                tmp = y.sibiling
-                y.sibiling = z
+                tmp = y.sibling
+                y.sibling = z
                 y = tmp
             else:
-                tmp = z.sibiling
-                z.sibiling = y
+                tmp = z.sibling
+                z.sibling = y
                 z = tmp
 
         return H
 
-    def display(self,H):
-        if (H == None):
-            print("Heap vuoto")
-        p = H
-        while (p != None):
-            print("{0} ".format(p.n))
-            p = p.sibiling
-        print("\n")
-
-    def search(self, H, k):
-        x = H
+    def search(self, k):
+        x = self.H
         p = None
         if (x.n == k):
             p = x
             return p
         if (x.child != None and p == None):
             p = self.search(x.child,k)
-        if (x.sibiling != None and p == None):
-            p = self.search(x.sibiling,k)
+        if (x.sibling != None and p == None):
+            p = self.search(x.sibling,k)
 
         return p
 
-    def info(self, H):
+    def revert_list(self,y):
+        x = y
+        prev = tmp = None
+        while (x != None):
+            x.parent = None
+            next = x.sibling
+            x.sibling = prev
+            prev = x
+            x = next
+        y = prev
+
+        self.Hr = y
+        # self.info(self.Hr)
+
+
+    def extractMin(self, H1 = None):
+        if H1 == None:
+            H1 = self.H
+
+        t = None
+        self.Hr = None
+        x = H1
+        if (x == None):
+            print("Heap vuoto")
+            return x
+        min = x.n
+        p = x
+        while (p.sibling != None):
+            if (p.sibling.n < min):
+                min = p.sibling.n
+                t = p
+                x = p.sibling
+            p = p.sibling
+        if (t == None and x.sibling == None):
+            H1 = None
+        elif (t == None):
+            H1 = x.sibling
+        elif (t.sibling == None):
+            t = None
+        else:
+            t.sibling = x.sibling
+        if (x.child != None):
+            self.revert_list(x.child)
+            # x.child.sibling = None
+        self.union(H1,self.Hr)
+        return x
+
+
+
+    def info(self, H = None):
+        if (H == None):
+            H = self.H
         x = H
         c = s = p = "None"
         if x:
             if x.child:
                 c = x.child.n
-            if x.sibiling:
-                s = x.sibiling.n
+            if x.sibling:
+                s = x.sibling.n
             if x.parent:
                 p = x.parent.n
             print("n:{0} c:{1} s:{2} p:{3}".format(x.n,c,s,p))
         if (x.child != None):
             p = self.info(x.child)
-        if (x.sibiling != None):
-            p = self.info(x.sibiling)
+        if (x.sibling != None):
+            p = self.info(x.sibling)
 
 
-    def generateDot(self, H):
+    def generateDot(self,H):
         text = ""
         if (H == None):
             return text
@@ -144,30 +203,31 @@ class BinomialHeap:
 
         if (p.parent != None):
             text+="{0}->{1}\n".format(p.parent.n,p.n)
-        if (p.sibiling != None):
-            text=self.generateDot(p.sibiling)+text
+        if (p.sibling != None):
+            text=self.generateDot(p.sibling)+text
         if (p.child != None):
             text+=self.generateDot(p.child)
         if (p.parent == None):
             text="{0} \n".format(p.n)+text
         return text
 
-    def visualizeTree(self,H):
-        dot_text = DOT % self.generateDot(H)
+    def visualizeTree(self, name = "graph"):
+        dot_text = DOT % self.generateDot(self.H)
         dot = Source(dot_text)
-        dot.render("graph","data/cache",format="png")
+        dot.render(name,"data/cache",format="png")
 
 
 
 BH = BinomialHeap()
-H = BH.initializeHeap()
-H = BH.insert(H,1)
-H = BH.insert(H,3)
-H = BH.insert(H,5)
-H = BH.insert(H,6)
-H = BH.insert(H,9)
-H = BH.insert(H,2)
-H = BH.insert(H,11)
-H = BH.insert(H,4)
-print(BH.generateDot(H))
-BH.visualizeTree(H)
+
+BH.insert(1)
+BH.insert(3)
+BH.insert(5)
+BH.insert(6)
+BH.insert(9)
+BH.insert(2)
+BH.insert(11)
+BH.insert(4)
+BH.insert(19)
+BH.extractMin()
+BH.visualizeTree()
